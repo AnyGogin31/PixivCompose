cb256810c8658752ebcbf2c937e232e725dea597
AnyGogin31 <148564043+AnyGogin31@users.noreply.github.com>
2024-11-24 10:29:06 +0300

    feat(build-logic): add reusable configurations and extensions for Gradle projects

---
+++ build-logic/src/main/java/neilt/mobile/convention/Configuration.kt
@@ -0,0 +1,48 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 AnyGogin31
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package neilt.mobile.convention
+
+import org.gradle.api.JavaVersion
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+object Configuration {
+
+    object Java {
+        val JAVA_TARGET = JvmTarget.JVM_11
+        val JAVA_VERSION = JavaVersion.VERSION_11
+    }
+
+    object Sdk {
+        const val COMPILE_SDK = 35
+        const val TARGET_SDK = 35
+        const val MIN_SDK = 24
+    }
+
+    object App {
+        const val APPLICATION_ID = "neilt.mobile.pixiv"
+        const val VERSION_NAME = "1.0.0-Develop01"
+        const val VERSION_CODE = 1
+    }
+}
