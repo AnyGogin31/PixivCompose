ac14d03330f342195d36837306c1410f4bfd1f0c
AnyGogin31 <148564043+AnyGogin31@users.noreply.github.com>
2024-11-30 15:09:09 +0300

    feat(navigation): add custom navigation types and actions with serialization support

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -1,11 +1,15 @@
 plugins {
     alias(libs.plugins.neilt.mobile.android.library)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 dependencies {
 
     // Navigation
     implementation(libs.compose.navigation)
+
+    // Kotlin
+    implementation(libs.kotlin.serialization.json)
 }
 
 android {
