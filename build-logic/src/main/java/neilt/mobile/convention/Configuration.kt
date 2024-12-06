d5921de099c70c47279f4ac2d272191f554e87b0
AnyGogin31 <148564043+AnyGogin31@users.noreply.github.com>
2024-12-06 21:50:51 +0300

    build(version): update app version to 1.0.0-PP (code 4)

--- build-logic/src/main/java/neilt/mobile/convention/Configuration.kt
+++ build-logic/src/main/java/neilt/mobile/convention/Configuration.kt
@@ -42,7 +42,7 @@ object Configuration {
 
     object App {
         const val APPLICATION_ID = "neilt.mobile.pixiv"
-        const val VERSION_NAME = "1.0.0"
-        const val VERSION_CODE = 3
+        const val VERSION_NAME = "1.0.0-PP"
+        const val VERSION_CODE = 4
     }
 }
