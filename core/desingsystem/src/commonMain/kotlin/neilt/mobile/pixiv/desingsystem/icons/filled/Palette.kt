dafe3125fa52f6a3f1f5f76960df3a5a3e0487d9
AnyGogin31 <148564043+AnyGogin31@users.noreply.github.com>
2024-12-09 22:05:11 +0300

    feat(settings): add icons for upcoming settings screen

---
+++ core/desingsystem/src/commonMain/kotlin/neilt/mobile/pixiv/desingsystem/icons/filled/Palette.kt
@@ -0,0 +1,104 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 AnyGogin31
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package neilt.mobile.pixiv.desingsystem.icons.filled
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import neilt.mobile.pixiv.desingsystem.icons.IconDefaults.PathFillBrush
+import neilt.mobile.pixiv.desingsystem.icons.PixivIcons
+
+val PixivIcons.Filled.Palette: ImageVector by lazy(LazyThreadSafetyMode.NONE) {
+    ImageVector.Builder(
+        defaultWidth = 24.dp,
+        defaultHeight = 24.dp,
+        viewportWidth = 960f,
+        viewportHeight = 960f,
+    ).path(fill = PathFillBrush) {
+        moveTo(480.0f, 880.0f)
+        quadToRelative(-82.0f, 0.0f, -155.0f, -31.5f)
+        reflectiveQuadToRelative(-127.5f, -86.0f)
+        quadTo(143.0f, 708.0f, 111.5f, 635.0f)
+        reflectiveQuadTo(80.0f, 480.0f)
+        quadToRelative(0.0f, -83.0f, 32.5f, -156.0f)
+        reflectiveQuadToRelative(88.0f, -127.0f)
+        quadTo(256.0f, 143.0f, 330.0f, 111.5f)
+        reflectiveQuadTo(488.0f, 80.0f)
+        quadToRelative(80.0f, 0.0f, 151.0f, 27.5f)
+        reflectiveQuadToRelative(124.5f, 76.0f)
+        quadToRelative(53.5f, 48.5f, 85.0f, 115.0f)
+        reflectiveQuadTo(880.0f, 442.0f)
+        quadToRelative(0.0f, 115.0f, -70.0f, 176.5f)
+        reflectiveQuadTo(640.0f, 680.0f)
+        horizontalLineToRelative(-74.0f)
+        quadToRelative(-9.0f, 0.0f, -12.5f, 5.0f)
+        reflectiveQuadToRelative(-3.5f, 11.0f)
+        quadToRelative(0.0f, 12.0f, 15.0f, 34.5f)
+        reflectiveQuadToRelative(15.0f, 51.5f)
+        quadToRelative(0.0f, 50.0f, -27.5f, 74.0f)
+        reflectiveQuadTo(480.0f, 880.0f)
+        close()
+        moveTo(260.0f, 520.0f)
+        quadToRelative(26.0f, 0.0f, 43.0f, -17.0f)
+        reflectiveQuadToRelative(17.0f, -43.0f)
+        quadToRelative(0.0f, -26.0f, -17.0f, -43.0f)
+        reflectiveQuadToRelative(-43.0f, -17.0f)
+        quadToRelative(-26.0f, 0.0f, -43.0f, 17.0f)
+        reflectiveQuadToRelative(-17.0f, 43.0f)
+        quadToRelative(0.0f, 26.0f, 17.0f, 43.0f)
+        reflectiveQuadToRelative(43.0f, 17.0f)
+        close()
+        moveTo(380.0f, 360.0f)
+        quadToRelative(26.0f, 0.0f, 43.0f, -17.0f)
+        reflectiveQuadToRelative(17.0f, -43.0f)
+        quadToRelative(0.0f, -26.0f, -17.0f, -43.0f)
+        reflectiveQuadToRelative(-43.0f, -17.0f)
+        quadToRelative(-26.0f, 0.0f, -43.0f, 17.0f)
+        reflectiveQuadToRelative(-17.0f, 43.0f)
+        quadToRelative(0.0f, 26.0f, 17.0f, 43.0f)
+        reflectiveQuadToRelative(43.0f, 17.0f)
+        close()
+        moveTo(580.0f, 360.0f)
+        quadToRelative(26.0f, 0.0f, 43.0f, -17.0f)
+        reflectiveQuadToRelative(17.0f, -43.0f)
+        quadToRelative(0.0f, -26.0f, -17.0f, -43.0f)
+        reflectiveQuadToRelative(-43.0f, -17.0f)
+        quadToRelative(-26.0f, 0.0f, -43.0f, 17.0f)
+        reflectiveQuadToRelative(-17.0f, 43.0f)
+        quadToRelative(0.0f, 26.0f, 17.0f, 43.0f)
+        reflectiveQuadToRelative(43.0f, 17.0f)
+        close()
+        moveTo(700.0f, 520.0f)
+        quadToRelative(26.0f, 0.0f, 43.0f, -17.0f)
+        reflectiveQuadToRelative(17.0f, -43.0f)
+        quadToRelative(0.0f, -26.0f, -17.0f, -43.0f)
+        reflectiveQuadToRelative(-43.0f, -17.0f)
+        quadToRelative(-26.0f, 0.0f, -43.0f, 17.0f)
+        reflectiveQuadToRelative(-17.0f, 43.0f)
+        quadToRelative(0.0f, 26.0f, 17.0f, 43.0f)
+        reflectiveQuadToRelative(43.0f, 17.0f)
+        close()
+    }.build()
+}
