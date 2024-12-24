af1bcefe85d900f221768ca8f1738102b49eb94c
AnyGogin31 <148564043+AnyGogin31@users.noreply.github.com>
2024-12-24 15:42:59 +0300

    build(version): update app version to 1.1.2-rc01 (code 7)

--- build-logic/src/main/java/neilt/mobile/convention/Configuration.kt
+++ build-logic/src/main/java/neilt/mobile/convention/Configuration.kt
@@ -42,7 +42,7 @@ object Configuration {
 
     object App {
         const val APPLICATION_ID = "neilt.mobile.pixiv"
-        const val VERSION_NAME = "1.1.1"
-        const val VERSION_CODE = 6
+        const val VERSION_NAME = "1.1.2-rc01"
+        const val VERSION_CODE = 7
     }
 }
