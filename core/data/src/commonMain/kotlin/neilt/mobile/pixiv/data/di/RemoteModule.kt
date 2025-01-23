3c65f008e2558cc717f63ac9f77c81cc6b39bfa8
AnyGogin31 <148564043+AnyGogin31@users.noreply.github.com>
2025-01-23 15:45:34 +0300

    feat: integrate Logback for enhanced logging in core data module

--- core/data/src/commonMain/kotlin/neilt/mobile/pixiv/data/di/RemoteModule.kt
+++ core/data/src/commonMain/kotlin/neilt/mobile/pixiv/data/di/RemoteModule.kt
@@ -28,8 +28,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
@@ -61,15 +59,7 @@ private fun provideHttpClient(
     tokenProvider: TokenProvider? = null,
 ): HttpClient {
     return HttpClient(CIO) {
-        install(Logging) {
-            logger = object : Logger {
-                override fun log(message: String) {
-                    println(message)
-                }
-            }
-            level = LogLevel.ALL
-        }
-
+        install(Logging)
         install(ContentNegotiation) {
             json(
                 Json {
